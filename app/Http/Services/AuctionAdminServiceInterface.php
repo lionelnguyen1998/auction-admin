e8d96bab085d6a79c1f41de1de14d4f80f6e0b4b
lionelnguyen1998 <lionelnguyen1998@gmail.com>
2022-02-18 10:11:14 +0700

    feature/home

--- app/Http/Services/AuctionAdminServiceInterface.php
+++ app/Http/Services/AuctionAdminServiceInterface.php
@@ -6,4 +6,10 @@ interface AuctionAdminServiceInterface
 {
     public function getListAuctions();
     public function getDetailAuctions($datas);
+    public function getSellingUser($datas);
+    public function getMaxPrice($datas);
+    public function getBids($datas);
+    public function getComments($datas);
+    public function getInfor($datas);
+    public function getCategoryValueName($datas);
 }
