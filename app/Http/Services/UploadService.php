47a8cd4eef7c234a6b998235f1e8713eacad9725
Nguyen Thi Tra <lionelnguyen1998@gmail.com>
2022-05-06 08:51:57 +0700

    fixbug

--- app/Http/Services/UploadService.php
+++ app/Http/Services/UploadService.php
@@ -13,6 +13,15 @@ class UploadService implements UploadServiceInterface
                 $result = $request->file('file')->storeOnCloudinary();
                 $url = $result->getPath();
                 return $url;
+                // $name = $request->file('file')->getClientOriginalName();
+                // $pathFull = 'uploads/' . date("Y/m/d");
+                // $request->file('file')->storeAs(
+                //     'public/' . $pathFull, $name
+                // );
+
+                // $url = 'http://admin.localhost:443/';
+                
+                // return $url . '/storage/' . $pathFull . '/' . $name;
             } catch (\Exception $error) {
                 return false;
             }
