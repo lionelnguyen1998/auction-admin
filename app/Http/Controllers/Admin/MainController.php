f46b8fb9e1206875ae4fa3bff62fee2069df0826
Nguyen Thi Tra <lionelnguyen1998@gmail.com>
2022-05-13 00:43:08 +0700

    fixbug

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -20,7 +20,42 @@ class MainController extends Controller
         $auction = Auction::get()
             ->toArray();
         if ($auction) {
-            Auction::updateStatus();
+            $auctions = Auction::where('auction_id', '<>', 3)
+            ->get();
+        
+        foreach ($auctions as $key => $value) {
+            $auction = Auction::findOrFail($value->auction_id);
+            if ($auction && ($value->status != 4) && ($value->status != 6)) {
+                if ($value->start_date <= now() && $value->end_date > now()) {
+                    $auction->status = 1;
+                    $auction->update();
+                } elseif ($value->end_date <= now()) {
+                    $auction->status = 3;
+                    $auction->update();
+                } else {
+                    $auction->status = 2;
+                    $auction->update();
+                }
+            }
+                
+            if (($value->status == 4) && ($value->end_date < now())) {
+                $auction->status = 5;
+                $auction->reason = 'Đã quá thời gian duyệt';
+                $auction->update();
+
+                $itemId = Item::where('auction_id', '=', $value->auction_id)
+                    ->get()
+                    ->pluck('item_id')
+                    ->toArray();
+
+                if (isset($itemId[0])) {
+                    Image::where('item_id', '=', $itemId[0])->delete();
+                    Item::where('item_id', '=', $itemId[0])->delete();
+                }
+    
+                Auction::findOrFail($value->auction_id)->delete();
+            }
+        }
         }
         return view('admin.home', [
             'title' => 'ホームページ',
