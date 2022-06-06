7d93c6eeb0b3c08ca80f4bfc66bd953a40476d08
Nguyen Thi Tra <lionelnguyen1998@gmail.com>
2022-06-06 09:32:58 +0700

    fixbug

---
+++ database/migrations/2022_06_05_094137_create_rate_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rate', function (Blueprint $table) {
+            $table->increments('rate_id');
+            $table->unsignedInteger('buying_user_id');
+            $table->unsignedInteger('auction_id');
+            $table->tinyInteger('star');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rate');
+    }
+}
